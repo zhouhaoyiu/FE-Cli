95f33cdf5dea30383919dd1e70515c12c6c8f8a2
zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>
2022-06-03 17:53:35 +0800

    some changes
    Signed-off-by: zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>

--- scripts/husky_init.mjs
+++ scripts/husky_init.mjs
@@ -1,19 +1,19 @@
 import execa from 'execa';
 import fs from 'fs';
 
-// 不输出\377\376
+
 
 const info = `#!/usr/bin/env sh
 . "$(dirname -- "$0")/_/husky.sh"
 
 npm run build
-`.replace(/\r?\n/g, '\n');
+`;
 
 //检查是否有.husky/pre-commit文件
 if(!fs.existsSync('./.husky/pre-commit')){
     //没有则创建
     execa("npx", ["husky", "add", ".husky/pre-commit", "npm run build"]);
 }else{
-    //有则覆盖
-    fs.writeFileSync('./.husky/pre-commit', info);
+    //有则覆盖,使用utf8编码，没有bom头
+    fs.writeFileSync('./.husky/pre-commit', info, {encoding: 'utf8', flag: 'w'});
 }
